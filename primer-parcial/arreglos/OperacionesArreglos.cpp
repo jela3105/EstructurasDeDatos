2043574a361d58ca2d07474a980984062c001f95
jela3105 <jela3105@gmail.com>
2022-02-09 10:50:28 -0600

    Agregadas ultimas funciones

--- primer-parcial/arreglos/OperacionesArreglos.cpp
+++ primer-parcial/arreglos/OperacionesArreglos.cpp
@@ -8,7 +8,7 @@ int main() {
 
   int datos[20] = {0};
   int opcion, n, suma, maximo, minimo, subinidice, dato_buscar, encontrado,
-      valor, longitud_arreglo = 0;
+      valor, elementos_arreglo = 0;
   float promedio;
   srand(time(NULL));
 
@@ -25,9 +25,10 @@ int main() {
     cout << "7.  Pomedio\n";
     cout << "8.  Mayores al promedio\n";
     cout << "9.  Inicializacion con N\n";
-    /*
-    cout << "6.  Busqueda\n"
-    */
+    cout << "10. Insertar al inicio\n";
+    cout << "11. Insertar un bloque al inicio\n";
+    cout << "12. Insertar al final\n";
+
     cout << "30. Salir\n";
     cout << "    Selecciona una opcion: ";
     cin >> opcion;
@@ -43,8 +44,8 @@ int main() {
     case 1:
       cout << "    Cuantos datos deseas ingresar?: ";
       cin >> n;
-      if (n > longitud_arreglo)
-        longitud_arreglo = n;
+      if (n > elementos_arreglo)
+        elementos_arreglo = n;
       if (n <= 20) {
         for (int i = 0; i < n; i++) {
           cout << "    Ingresa el alor de datos[" << i << "]: ";
@@ -58,8 +59,8 @@ int main() {
     case 2:
       cout << "    Cuantos datos aleatoreos desea?: ";
       cin >> n;
-      if (n > longitud_arreglo)
-        longitud_arreglo = n;
+      if (n > elementos_arreglo)
+        elementos_arreglo = n;
       if (n <= 20) {
         for (int i = 0; i < n; i++) {
           datos[i] = 1 + rand() % 100;
@@ -121,23 +122,22 @@ int main() {
 
     case 7:
       suma = 0;
-      for (int i = 0; i < longitud_arreglo; i++) {
+      for (int i = 0; i < elementos_arreglo; i++) {
         suma += datos[i];
       }
-      promedio = (float)suma / longitud_arreglo;
+      promedio = (float)suma / elementos_arreglo;
       cout << "\n    El promedio es: " << promedio << endl;
       break;
 
     case 8:
       suma = 0;
-      for (int i = 0; i < longitud_arreglo; i++) {
+      for (int i = 0; i < elementos_arreglo; i++) {
         suma += datos[i];
       }
-      promedio = (float)suma / longitud_arreglo;
-
+      promedio = (float)suma / elementos_arreglo;
       cout << "\n    Los valores mayores al promedio son: " << endl;
       cout << "    Posicion		Valor " << endl;
-      for (int i = 0; i < longitud_arreglo; i++) {
+      for (int i = 0; i < elementos_arreglo; i++) {
         if (datos[i] > promedio) {
           cout << "    " << i << "\t\t\t" << datos[i] << endl;
         }
@@ -147,13 +147,62 @@ int main() {
     case 9:
       cout << "\n    Cuantos datos deseas ingresar?: ";
       cin >> n;
-      if (n > longitud_arreglo)
-        longitud_arreglo = n;
+      if (n > elementos_arreglo)
+        elementos_arreglo = n;
       cout << "\n    Cual es el valor con el que iniciaran?: ";
       cin >> valor;
       for (int i = 0; i < n; i++) {
         datos[i] = valor;
       }
+      break;
+
+    case 10:
+      cout << "\n    Ingrese el numero: ";
+      cin >> valor;
+      for (int i = elementos_arreglo; i > 0; i--) {
+        datos[i] = datos[i - 1];
+      }
+      datos[0] = valor;
+      if (elementos_arreglo < 20)
+        elementos_arreglo++;
+      break;
+
+    case 11:
+      cout << "\n    Ingrese el numero de elementos a agregar: ";
+      cin >> n;
+      if (elementos_arreglo + n <= 20) {
+        for (int i = 0; i < n; i++) {
+          datos[i + n] = datos[i];
+          cout << "    Ingresa dato[" << i << "]: ";
+          cin >> datos[i];
+        }
+        elementos_arreglo += n;
+      } else
+        cout << "\n    No pueden haber mas de 20 elementos";
+      break;
+
+    case 12:
+      cout << "\n    Ingrese el numero: ";
+      cin >> valor;
+      if (elementos_arreglo < 20) {
+        datos[elementos_arreglo] = valor;
+        elementos_arreglo++;
+      } else
+        datos[19] = valor;
+      break;
+
+    case 13:
+      cout << "\n    Ingrese el numero de elementos a ingresar: ";
+      cin >> n;
+      if (elementos_arreglo + n <= 20) {
+        for (int i = 0; i < n; i++) {
+          cout << "Ingrese el elemento datos[" << elementos_arreglo + i
+               << "]: ";
+          cin >> datos[elementos_arreglo + i];
+        }
+        elementos_arreglo += n;
+      } else
+        cout << "\n    No pueden haber mas de 20 elementos";
       break;
 
     case 30:
