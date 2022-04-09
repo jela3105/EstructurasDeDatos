7a79b1314c97b30b8eeab2cf56c5c3f97c87636f
jela3105 <jela3105@gmail.com>
2022-04-09 10:17:09 -0500

    Creado menu inicial

--- segundo-parcial/reproductor/reproductor-musica.cpp
+++ segundo-parcial/reproductor/reproductor-musica.cpp
@@ -6,11 +6,35 @@ struct cancion {
   struct cancion *anterior, *siguiente;
 };
 
+bool reproducirCancion(struct cancion) {}
+
 int main() {
+  int opcion;
   while (true) {
-    cout << "0. Imprimir de izquierda a derecha" << endl;
-    cout << "1. Imprimir de derecha a izquierda" << endl;
-    cout << "2. Insertar a la derecha" << endl;
-    cout << "Ingresa una opcion: ";
+    cout << "1. Lista de reproduccion" << endl;
+    cout << "2. Siguiente" << endl;
+    cout << "3. Anterior" << endl;
+    cout << "4. Reproduccion aleatoria" << endl;
+    cout << "5. Eliminar cancion" << endl;
+    cout << "6. Agregar cancion" << endl;
+    cout << "7. Buscar" << endl;
+    cout << "Seleccione una opcion: ";
+    cin >> opcion;
+    switch (opcion) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    }
   }
 }
