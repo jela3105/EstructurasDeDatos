754d8eeb73ab640a1f5a652725defc9e4ab3d709
jela3105 <jela3105@gmail.com>
2022-05-28 22:18:42 -0500

    Read and print the graph

--- tercer-parcial/dijkstra.cpp
+++ tercer-parcial/dijkstra.cpp
@@ -1,4 +1,53 @@
 #include <iostream>
+#include <set>
+#include <unordered_map>
 #include <vector>
 
-int main() { return 0; }
+using namespace std;
+
+void readGraph(vector<vector<int>> &, unordered_map<int, char> &);
+void printAdjacencyMatrix(vector<vector<int>>);
+
+int main() {
+  int n;
+  cin >> n;
+  unordered_map<int, char> vertex_map;
+  vector<vector<int>> graph(n, vector<int>(n, -1));
+  readGraph(graph, vertex_map);
+  printAdjacencyMatrix(graph);
+  return 0;
+}
+
+void readGraph(vector<vector<int>> &graph, unordered_map<int, char> &v_m) {
+  unordered_map<char, int> vertex_read;
+  char vertex_origin, vertex_destiny;
+  int n, cost, vertex_counter = 0;
+  cin >> n;
+  cout << n << endl;
+  for (int i = 0; i < n; i++) {
+    cin >> vertex_origin;
+    if (vertex_read.find(vertex_origin) == vertex_read.end()) {
+      vertex_read[vertex_origin] = vertex_counter;
+      v_m[vertex_counter] = vertex_origin;
+      vertex_counter++;
+    }
+    cin >> vertex_destiny;
+    if (vertex_read.find(vertex_destiny) == vertex_read.end()) {
+      vertex_read[vertex_destiny] = vertex_counter;
+      v_m[vertex_counter] = vertex_destiny;
+      vertex_counter++;
+    }
+    cin >> cost;
+    graph[vertex_read[vertex_origin]][vertex_read[vertex_destiny]] = cost;
+    graph[vertex_read[vertex_destiny]][vertex_read[vertex_origin]] = cost;
+  }
+}
+
+void printAdjacencyMatrix(vector<vector<int>> matrix) {
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[0].size(); j++) {
+      cout << matrix[i][j] << "  ";
+    }
+    cout << endl;
+  }
+}
