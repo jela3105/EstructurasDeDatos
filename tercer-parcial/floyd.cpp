24d42b99361d23f3574f764b10aa06f7e9b0e179
jela3105 <jela3105@gmail.com>
2022-06-05 18:52:41 -0500

    Finish floyd algorthm

--- tercer-parcial/floyd.cpp
+++ tercer-parcial/floyd.cpp
@@ -13,6 +13,8 @@ void readGraph(vector<vector<int>> &, unordered_map<int, char> &,
 void printMatrix(vector<vector<int>>);
 void initializeRoutes(vector<vector<int>> &);
 void floyd(vector<vector<int>> &, vector<vector<int>> &);
+vector<int> findPath(vector<vector<int>>, int, int);
+void printRoutesMatrix(vector<vector<int>>, unordered_map<int, char>);
 
 int main() {
   int n;
@@ -22,14 +24,31 @@ int main() {
   vector<vector<int>> graph(n, vector<int>(n, infinite));
   vector<vector<int>> routes(n, vector<int>(n, infinite));
   readGraph(graph, int_to_char, char_to_int);
-  printMatrix(graph);
   vector<vector<int>> costs = graph;
   initializeRoutes(routes);
   floyd(costs, routes);
+  printMatrix(graph);
   cout << endl;
   printMatrix(costs);
   cout << endl;
-  printMatrix(routes);
+  printRoutesMatrix(routes, int_to_char);
+  cout << endl;
+  while (true) {
+    char origin, destiny;
+    cout << endl << "Ingresa el nodo origen: ";
+    cin >> origin;
+    cout << "Ingresa el nodo destino: ";
+    cin >> destiny;
+    cout << "El costo es de: "
+         << costs[char_to_int[origin]][char_to_int[destiny]] << endl;
+    vector<int> path =
+        findPath(routes, char_to_int[origin], char_to_int[destiny]);
+    cout << "El camino es: " << origin << " ";
+    for (int node : path) {
+      cout << int_to_char[node] << " ";
+    }
+    cout << destiny << endl;
+  }
 }
 
 void readGraph(vector<vector<int>> &graph, unordered_map<int, char> &v_m,
@@ -73,6 +92,17 @@ void printMatrix(vector<vector<int>> matrix) {
   }
 }
 
+void printRoutesMatrix(vector<vector<int>> matrix,
+                       unordered_map<int, char> i_c) {
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[0].size(); j++) {
+      matrix[i][j] == infinite ? cout << "*\t"
+                               : cout << i_c[matrix[i][j]] << "\t";
+    }
+    cout << endl;
+  }
+}
+
 void initializeRoutes(vector<vector<int>> &routes) {
   for (int i = 0; i < routes.size(); i++) {
     for (int j = 0; j < routes.size(); j++) {
@@ -103,4 +133,14 @@ void floyd(vector<vector<int>> &costs, vector<vector<int>> &routes) {
       }
     }
   }
+}
+
+vector<int> findPath(vector<vector<int>> routes, int origin, int destiny) {
+  int position = destiny;
+  vector<int> path;
+  while (routes[origin][position] != position) {
+    path.insert(path.begin(), routes[origin][position]);
+    position = routes[origin][position];
+  }
+  return path;
 }
